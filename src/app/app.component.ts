b4184ec2de589ade7ac7b04a002e5b4292f23e05
Pavel <103207482+Fruerdd@users.noreply.github.com>
2025-04-03 17:33:00 +0200

    fix: app.module and home.module deleted
    - No need, while we are using Standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './features/header/header.component';  
-import { FooterComponent } from './features/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; 
+import { HeaderComponent } from 'src/app/features/header/header.component'; 
+import { FooterComponent } from 'src/app/features/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent]  
+  imports: [
+    CommonModule,
+    RouterModule,
+    HeaderComponent,
+    FooterComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title(title: any) {
-    throw new Error('Method not implemented.');
-  }
 }
