7e593083975a4e78e8dea0d04f174eac11098bb5
Pavel <103207482+Fruerdd@users.noreply.github.com>
2025-06-07 20:22:21 +0200

    Fix(home testimonials view/audio view in mobile view)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://smart-shopping-list-api.onrender.com' // Needs changing when deployed
+    apiUrl: 'http://localhost:8080' // Needs changing when deployed
 }
+
+
+// https://smart-shopping-list-api.onrender.com
